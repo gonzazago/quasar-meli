a36edd37a81335690cc265d3bb72e33bc5af2a4e
Gonzalo Zago <gzago@santandertecnologia.com.ar>
2021-01-13 17:04:31 -0300

    add properties from heroku

--- src/main/java/com/meli/quasar/handler/ComunicationHandler.java
+++ src/main/java/com/meli/quasar/handler/ComunicationHandler.java
@@ -1,28 +1,16 @@
 package com.meli.quasar.handler;
 
-import com.meli.quasar.config.SatelliteConfig;
-import com.meli.quasar.dtos.SatelliteDTO;
 import com.meli.quasar.dtos.TopSecretRequestDTO;
 import com.meli.quasar.dtos.TopSecretResponseDTO;
-import com.meli.quasar.entities.Position;
-import com.meli.quasar.entities.Satellite;
 import com.meli.quasar.services.ComunicationService;
-import com.meli.quasar.services.LocationService;
-import com.meli.quasar.services.MessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.meli.quasar.constants.StringContstants.SATELLITE_KENOBY;
 
 @Component
 @Slf4j
